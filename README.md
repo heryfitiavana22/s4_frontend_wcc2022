dc2748aa94ca99826482961dd65c38b1d2f6103f
heryfitiavana22 <hheryfitiavana@gmail.com>
2022-09-24 15:52:22 +0300

    section left doing

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # s4_frontend_wcc2022
+
+## tz_wcc22
---
+++ js/app.js
@@ -0,0 +1,8 @@
+let listNav = document.querySelector('.nav-list');
+function showNav() {
+    listNav.style.left = '0'
+}
+
+function hideNav() {
+    listNav.style.left = '100%'
+}
